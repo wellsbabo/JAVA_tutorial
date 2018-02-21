3ee4c6c15484fa6fce40c7e1afcef760ffb52df1
wellsbabo <wellsbabo@gmail.com>
2018-02-21 11:52:11 +0900

    Create 메소드.java

---
+++ 메소드.java
@@ -0,0 +1,17 @@
+package javatutorials
+
+public class MethodDemo{
+
+  public static void numbering(int init, int limit){
+    int i = limit;
+    while(i < limit){
+      System.out.println(i);
+      i++;
+      }
+    }
+    
+    public static void main(String[] args){
+      numbering(1,5);
+      }
+ }
+    
