26014607425180b7f4954fb78b3a07e78b0dd753
wellsbabo <wellsbabo@gmail.com>
2018-02-21 11:57:06 +0900

    Update 메소드_기본.java

--- 메소드_기본.java
+++ 메소드_기본.java
@@ -2,7 +2,7 @@ package javatutorials
 
 public class MethodDemo{
 
-  public static void numbering(int init, int limit){
+  public static void numbering(int init, int limit){      //void반환값
     int i = limit;
     while(i < limit){
       System.out.println(i);
